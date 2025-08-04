db12ce95988f78c103d6b798f28f4b56a9253d31
zhaojingkai <1195525843@qq.com>
2025-08-04 18:20:43 +0800

    Init

--- system-manage/src/main/java/com/xmzoda/systemmanage/controller/FileController.java
+++ system-manage/src/main/java/com/xmzoda/systemmanage/controller/FileController.java
@@ -2,7 +2,6 @@ package com.xmzoda.systemmanage.controller;
 
 import com.xmzoda.systemmanage.common.Result;
 import com.xmzoda.systemmanage.request.FileRequest;
-import com.xmzoda.systemmanage.util.FileReaderUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FileController {
     @PostMapping("/readCSV")
     public Result readCsv(@RequestBody FileRequest fileRequest) {
-        String filename = "\\\\Desktop-07kteim\\共享文档-IT01\\sixi\\拼多多\\1-订单明细\\2025-02-01-2025-02-28";
-
-        String content = FileReaderUtil.readFileSafely(filename, "utf-8", "文件读取失败");
-        return Result.success(filename);
+        return Result.success("");
     }
 }
